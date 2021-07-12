1097e0a149f2e729924627166775f29fdf1b3d42
“wesleyMeli” <wesley.ferreira@mercadolivre.com>
2021-07-12 17:48:38 -0300

    :feat - ptojeto base

---
+++ src/main/java/com/digitalhouse/Modulo10/Modulo10Application.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.Modulo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo10Application.class, args);
+	}
+
+}
