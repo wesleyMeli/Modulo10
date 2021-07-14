c0dd607364e976287433332a883e77d428283ec4
“wesleyMeli” <wesley.ferreira@mercadolivre.com>
2021-07-14 17:23:24 -0300

    feat : US-CODE : ml-school-01 - criacao de metodos para teste integrado

--- src/test/java/com/digitalhouse/Modulo10/test/integrated/StudentControllerTest.java
+++ src/test/java/com/digitalhouse/Modulo10/test/integrated/StudentControllerTest.java
@@ -1,5 +1,6 @@
-package com.digitalhouse.Modulo10.teste;
+package com.digitalhouse.Modulo10.test.integrated;
 
+import com.digitalhouse.Modulo10.dto.DiplomaDTO;
 import com.digitalhouse.Modulo10.dto.StudentDTO;
 import com.digitalhouse.Modulo10.dto.SubjectDTO;
 import com.digitalhouse.Modulo10.repositry.StudentRepository;
@@ -7,23 +8,20 @@ import com.digitalhouse.Modulo10.service.StudentService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-@ExtendWith(SpringExtension.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
@@ -67,11 +65,27 @@ class StudentControllerTest {
     }
 
     @Test
+    void givenAStudentUnnamedWithThreeSubjectsGetABadRequestStatus() throws Exception {
+        studentDTO.setName(null);
+        mock.perform(post("/student/analyzeNotes").
+                contentType("application/json").content(mapper.writeValueAsString(studentDTO))).andExpect(status().isBadRequest());
+    }
+
+    @Test
     void givenAStudentWithThreeSubjectsGetABadRequestStatusWithADisapprovalMessage() throws Exception {
+        List<SubjectDTO> subjectDTOS = new ArrayList<>();
+        subjectDTOS.add(new SubjectDTO("quimica basica", BigDecimal.valueOf(4)));
+        subjectDTOS.add(new SubjectDTO("matematica", BigDecimal.valueOf(4)));
+        studentDTO.setMatters(subjectDTOS);
+        mock.perform(post("/student/analyzeNotes").
+                contentType("application/json").content(mapper.writeValueAsString(studentDTO))).
+                andExpect(status().isBadRequest()).andExpect(MockMvcResultMatchers.jsonPath("$.Reprovado").value("Estudante "+studentDTO.getName()+" foi reprovado com a media de "+new DiplomaDTO(studentDTO).getAvarage()));
+    }
+    @Test
+    void givenAStudentWithThreeSubjectsGetABadRequestStatusWithErrorMessageNotSubject() throws Exception {
         studentDTO.setMatters(new ArrayList<>());
         mock.perform(post("/student/analyzeNotes").
                 contentType("application/json").content(mapper.writeValueAsString(studentDTO))).
-                andExpect(status().isBadRequest()).andExpect(MockMvcResultMatchers.jsonPath("$.Reprovado").value("Estudante "+studentDTO.getName()+" foi reprovado com a media de 0.0"));
+                andExpect(status().isBadRequest()).andExpect(MockMvcResultMatchers.jsonPath("*").value("O estudante deve ter ao menos uma disciplina"));
     }
-
 }
