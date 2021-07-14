e9365bac87a4613635141d165df49aba02dfba5a
“wesleyMeli” <wesley.ferreira@mercadolivre.com>
2021-07-14 17:24:40 -0300

    feat : US-CODE : ml-school-01 - correcao de validacao

--- src/main/java/com/digitalhouse/Modulo10/dto/StudentDTO.java
+++ src/main/java/com/digitalhouse/Modulo10/dto/StudentDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -18,6 +19,7 @@ public class StudentDTO {
     private String name;
 
     @Valid
+    @NotEmpty(message = "O estudante deve ter ao menos uma disciplina")
     @JsonProperty("subjects")
     private List<SubjectDTO> matters;
 
